c06fa6ce81053f6840550ec86bcf7c5cd10949e8
jintu singha <jintumonisingha411@gmail.com>
2025-08-14 11:39:05 +0530

    feat:Added di plugins and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
@@ -64,4 +66,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.3")
 
     implementation("androidx.room:room-runtime:2.7.2")
+
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
 }
